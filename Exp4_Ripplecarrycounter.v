d05bcf52116b9d72638aa1eb4831078dfd2e2564
MANOJKUMAR282004 <167055290+MANOJKUMAR282004@users.noreply.github.com>
2024-05-11 14:10:39 +0530

    Exp4_Ripplecarrycounter.v

--- Exp4_Ripplecarrycounter.v
+++ Exp4_Ripplecarrycounter.v
@@ -1,5 +1,34 @@
 module ripple_carry_counter(q, clk, reset);
-output [3:0] q;
+output [3:0] q
+module tff(q,clk,rst);
+input clk,rst;
+output q;
+wire d;
+dff df1(q,d,clk,rst);
+not n1(d,q);
+endmodule
+
+module dff(q,d,clk,rst);
+input d,clk,rst;
+output q;
+reg q;
+always @(posedge clk or posedge rst)
+begin
+if (rst)
+q=1'b0;
+else 
+q=d;
+end
+endmodule
+
+module ripplecounter(clk,rst,q);
+input clk,rst;
+output [3:0]q;
+tff tf1(q[0],clk,rst);
+tff tf3(q[2],q[1],rst);
+tff tf4(q[3],q[2],rst);
+endmodule
+
 input clk, reset;
 T_FF tff0(q[0], clk, reset);
 T_FF tff1(q[1], q[0], reset);
